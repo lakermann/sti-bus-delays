1ca174bbeeb9ccc63e155c9c11218b867415f2fb
Lukas Akermann <lukas.akermann@zuehlke.com>
2024-08-19 21:12:36 +0200

    Initial commit

---
+++ test/sti-bus-delays.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as StiBusDelays from '../lib/sti-bus-delays-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/sti-bus-delays-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new StiBusDelays.StiBusDelaysStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
